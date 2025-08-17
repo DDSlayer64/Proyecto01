0ba819c1872c35dc2b551d7168dc7f71ed157038
DDSlayer64 <P.E.K.K.A12.13.31.13@gmail.com>
2025-08-17 12:28:55 -0300

    Se termino el desarrollo de la pagina Usuario, falta cambio de colores

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -1,21 +1,33 @@
 <script setup>
 import Inicio from './Inicio.vue';
+
+function mostrarAlerta(){
+    alert("Esta funcionaldidad aun no esta disponible")
+}
+
 </script>
 
 <template>
-    <div class="todo bg-amber-400 w-screen h-[90vh] flex justify-center items-center">
-        <div class="bg-blue-500 w-[50vw] h-[50%] flex">
-            <div class="w-[50%] h-[100%] flex items-left">
-                <div class="w-[100%] h-[50%]"><h3>Nombre y apellido</h3></div>
-                <div class="w-[100%] h-[50%]"><h3>Numero de contacto</h3></div>
+    <div class="todo bg-amber-400 w-screen h-[90vh] flex flex-col justify-evenly items-center ">
+        <div class="cajas bg-blue-500 w-[50vw] h-[50%] flex">
+            <div class="caja-01 w-[50%] h-[100%]">
+                <div class="w-[100%] h-[50%] flex justify-center items-center"><h3>Nombre y apellido</h3></div>
+                <div class="w-[100%] h-[50%] flex justify-center items-center"><h3>Numero de contacto</h3></div>
                
             </div>
-            <div class="w-[50%] h-[100%] ">
-                <div class="w-[100%] h-[50%]"><p>Correo electronico</p></div>
-                <div class="w-[100%] h-[50%]"><p>Rol</p></div>
+            <div class="caja-02 w-[50%] h-[100%]">
+                <div class="w-[100%] h-[50%] flex justify-center items-center"><p>Correo electronico</p></div>
+                <div class="w-[100%] h-[50%] flex justify-center items-center"><p>Rol</p></div>
             
                 
             </div>
         </div>
+
+
+         <div class="configuracion w-[50%] h-[20%] bg-red-400 flex justify-center">
+            <button type="submit" class="hover:bg-sky-700 active:bg-pink-200 placeholder:bg-black-500" 
+            @click="mostrarAlerta">Editar perfil</button>
+         </div>
+
     </div>
 </template>
