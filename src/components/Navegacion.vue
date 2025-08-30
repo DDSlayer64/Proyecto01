6994a96ccd709282bb6f3635b335a4096c5dab34
DDSlayer64 <P.E.K.K.A12.13.31.13@gmail.com>
2025-08-30 11:27:34 -0300

    Se agrego estilo a la pagina de mensajes

--- src/components/Navegacion.vue
+++ src/components/Navegacion.vue
@@ -4,16 +4,16 @@
 
 <template>
     <nav class="navegacion w-[100vw] h-[10vh] bg-green-400 flex flex-row">
-        <router-link to="inicio" class="hover:bg-amber-200 px-2">Ir a Inicio
+        <router-link to="inicio" class="hover:bg-green-300 px-2 border-2 border-green-600">Ir a Inicio
 
         </router-link>
-        <router-link to="segunda" class="hover:bg-amber-200 px-2">Ir a Segunda
+        <router-link to="segunda" class="hover:bg-green-300 px-2 border-2 border-green-600">Ir a Segunda
             
         </router-link>
-        <router-link to="usuario" class="hover:bg-amber-200 px-2">Ir a Usauario
+        <router-link to="usuario" class="hover:bg-green-300 px-2 border-2 border-green-600">Ir a Usauario
 
         </router-link>
-        <router-link to="usuario/mensajes" class="hover:bg-amber-200 px-2">Ir a Mensajes
+        <router-link to="usuario/mensajes" class="hover:bg-green-300 px-2 border-2 border-green-600">Ir a Mensajes
 
         </router-link>
     </nav>
--- src/views/Mensajes.vue
+++ src/views/Mensajes.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="p-6 bg-gray-100 min-h-screen">
+  <div class="p-6 bg-sky-200 min-h-screen">
     <h1 class="text-2xl font-bold mb-4">Mensajes enviados</h1>
 
     <!-- BOTONES -->
     <div class="mb-6 space-x-4">
-      <button @click="goToPrincipal" class="px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-500">
+      <button @click="goToPrincipal" class="px-4 py-2 bg-blue-900 text-white rounded hover:bg-sky-700 border-3 border-green-500">
         Ir a /principal
       </button>
-      <button @click="openModal" class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-400">
+      <button @click="openModal" class="px-4 py-2 bg-blue-900 text-white rounded hover:bg-sky-700 border-3 border-green-500">
         Crear nuevo mensaje
       </button>
     </div>
 
     <!-- LISTA DE MENSAJES -->
-    <div v-if="messages.length">
+    <div v-if="messages.length" >
       <div
         v-for="(msg, index) in messages"
         :key="index"
-        class="bg-white shadow-md p-4 rounded mb-4 border border-gray-300"
+        class="bg-blue-900 text-white shadow-md p-4 rounded-2xl mb-4 border-4 border-green-500"
       >
         <p><strong>Destinatario:</strong> {{ msg.destinatario }}</p>
         <p><strong>Título:</strong> {{ msg.titulo }}</p>
         <p><strong>Contenido:</strong> {{ msg.contenido }}</p>
-        <p class="text-sm text-gray-500">
+        <p class="text-sm text-white">
           <strong>Fecha y hora:</strong> {{ msg.fecha }}
         </p>
       </div>
     </div>
-    <div v-else class="text-gray-500">No hay mensajes enviados aún.</div>
+    <div v-else class="">No hay mensajes enviados aún.</div>
 
     <!-- MODAL -->
     <div v-if="showModal" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
